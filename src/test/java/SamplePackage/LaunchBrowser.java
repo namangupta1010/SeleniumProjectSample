7f4ee56e64fa7aad6117ce739be0af1cd9a1dcbf
naman <namangupta1010@gmail.com>
2023-11-22 16:58:27 +0530

    Commit for jenkins

--- src/test/java/SamplePackage/LaunchBrowser.java
+++ src/test/java/SamplePackage/LaunchBrowser.java
@@ -8,7 +8,9 @@ public class LaunchBrowser {
     public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
+
+
+        driver.get("https://www.amazon.com");
         System.out.println(driver.getTitle());
         driver.close();
     }
